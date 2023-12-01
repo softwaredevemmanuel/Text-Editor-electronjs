3e5c0630b51f425482e47f340c4f63215425c532
Okereke Emmanuel <eokereke47@gmail.com>
2023-12-01 21:42:26 +0100

    step2

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
 
 
 app.on('ready', () => {
     console.log("app is tready");
+    let win = new BrowserWindow({
+
+    })
+    win.loadFile('index.html')
 });
