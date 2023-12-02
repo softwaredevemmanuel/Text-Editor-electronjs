5a24f938fca09b52963e5f8b3b13794acd73c568
Okereke Emmanuel <eokereke47@gmail.com>
2023-12-02 11:23:37 +0100

    Specify location to save the file

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const electron = require('electron');
 const fs = require('fs');
-const { app, BrowserWindow, ipcMain } = electron;
-
+const { app, BrowserWindow, ipcMain, dialog } = electron;
+let win
 
 app.on('ready', () => {
     console.log("app is tready");
-    let win = new BrowserWindow({
+    win = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -15,13 +15,22 @@ app.on('ready', () => {
     win.loadFile('index.html')
 });
 
-ipcMain.on('save', (event, text)=>{
-    console.log(text)
-    fs.writeFile('samplefile.txt', text, (err)=>{
-        if(err){
-            console.log('There was an error', err)
-        }else{
-            console.log('File has been saved') 
-        }
-    })
-})
+ipcMain.on('save', (event, text) => {
+    // show dialog
+    const fullpath = dialog.showSaveDialogSync(win, { defaultPath: 'filename.txt' });
+
+    if (fullpath) {
+        console.log(fullpath);
+        fs.writeFile(fullpath, text, (err) => {
+            if (err) {
+                console.log('There was an error', err);
+            } else {
+                console.log('File has been saved');
+            }
+        });
+    } else {
+        console.log('Save operation canceled by the user');
+    }
+});
+
+
