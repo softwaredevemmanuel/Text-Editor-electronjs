d8826972dfa59d696b3bfcfc807bc48fbef15cc5
Okereke Emmanuel <eokereke47@gmail.com>
2023-12-02 10:39:20 +0100

    connecting index.html & main.js with ipc main &ipc

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+const { app, BrowserWindow, ipcMain } = electron;
 
 
 app.on('ready', () => {
     console.log("app is tready");
     let win = new BrowserWindow({
-
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
     })
     win.loadFile('index.html')
 });
+
+ipcMain.on('save', (event, text)=>{
+    console.log(text)
+})
