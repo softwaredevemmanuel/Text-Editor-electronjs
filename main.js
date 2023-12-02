6a295c1e8b4764f8a76353a32a7990475381f7b9
Okereke Emmanuel <eokereke47@gmail.com>
2023-12-02 10:48:09 +0100

    writing the text to a file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+const fs = require('fs');
 const { app, BrowserWindow, ipcMain } = electron;
 
 
@@ -16,4 +17,11 @@ app.on('ready', () => {
 
 ipcMain.on('save', (event, text)=>{
     console.log(text)
+    fs.writeFile('samplefile.txt', text, (err)=>{
+        if(err){
+            console.log('There was an error', err)
+        }else{
+            console.log('File has been saved') 
+        }
+    })
 })
